456aa54e02e2a74f5012203a034c14936c9aff4d
jason.jafari.m@gmail.com <jason.jafari.m@gmail.com>
2020-05-04 14:20:36 -0400

    if statement

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 var app = new Vue({
     el: '#app',
     data: {
-
         message: 'Heloo jason'
     }
 });
+app.message = "salam"
+
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+app2.message = 'some new message'
+
+
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        seen: true
+    }
+})
+
+app3.seen = false;
+app3.seen = true;
